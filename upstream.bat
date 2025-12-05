c235ba5cd6c30168061fefa8610475a74d5684f3
kernelevil <547622133@qq.com>
2025-12-05 11:52:22 +0800

    my info - Date: 2025-12-05 11-52

--- upstream.bat
+++ upstream.bat
@@ -4,6 +4,6 @@ for /f "tokens=1-4 delims=/ " %%a in ("%date%") do set date=%%a-%%b-%%c
 for /f "tokens=1-2 delims=:," %%a in ("%time%") do set time=%%a-%%b
 set datetime=%date% %time%
 git commit -m "my info - Date: %datetime%"
-git pull git@github.com:kernelevil/asm.github.io.git
-git push git@github.com:kernelevil/asm.github.io.git
+git pull 
+git push 
 pause
